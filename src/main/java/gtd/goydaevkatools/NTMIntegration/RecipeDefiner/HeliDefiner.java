ac682176272f364e0a933cece4af4ac46672776d
Texas <68874053+Texaso@users.noreply.github.com>
2026-05-11 15:24:04 +0200

    крафтики HeliDefiner.java

--- src/main/java/gtd/goydaevkatools/NTMIntegration/RecipeDefiner/HeliDefiner.java
+++ src/main/java/gtd/goydaevkatools/NTMIntegration/RecipeDefiner/HeliDefiner.java
@@ -1,7 +1,20 @@
 package gtd.goydaevkatools.NTMIntegration.RecipeDefiner;
 
+import com.hbm.blocks.ModBlocks;
+import com.hbm.inventory.RecipesCommon;
+import com.hbm.items.ModItems;
+import com.hbm.items.weapon.sedna.factory.GunFactory;
+import gtd.goydaevkatools.ModItems.GTDITems;
+import gtd.goydaevkatools.NTMIntegration.VESSEL_TYPE;
+import gtd.goydaevkatools.NTMIntegration.VesselNTMRecipe;
+import gtd.goydaevkatools.VesselsComponents.TankComponentEnum;
 import mcheli.aircraft.MCH_AircraftInfoManager;
 import mcheli.helicopter.MCH_HeliInfoManager;
+import net.minecraft.item.Item;
+
+import static com.hbm.inventory.OreDictManager.AL;
+import static com.hbm.inventory.OreDictManager.STEEL;
+import static com.hbm.inventory.OreDictManager.TI;
 
 public class HeliDefiner extends AbstractDefiner
 {
@@ -20,6 +33,25 @@ public class HeliDefiner extends AbstractDefiner
 
     @Override
     void AbstractDefineProcedure() {
-
+        DefineRecipe(new VesselNTMRecipe(
+            "mi8amtsh-2",VESSEL_TYPE.HELI,100, 100,
+            new RecipesCommon.OreDictStack(AL.plateCast(), 22),
+            new RecipesCommon.OreDictStack(TI.plateCast(), 16),
+            new RecipesCommon.ComparableStack(GTDITems.wheel,3),
+            new RecipesCommon.ComparableStack(hbm:item.blade_titanium,8),
+            new RecipesCommon.ComparableStack(ModItems.motor, 8),
+            new RecipesCommon.OreDictStack(STEEL.lightBarrel(), 2),
+            new RecipesCommon.ComparableStack(ModItems.weapon_mod_special, 2,GunFactory.EnumModSpecial.ENGINE_DIESEL)
+        ));
+        DefineRecipe(new VesselNTMRecipe(
+            "mh60g",VESSEL_TYPE.HELI,100, 100,
+            new RecipesCommon.OreDictStack(AL.plateCast(), 32),
+            new RecipesCommon.OreDictStack(TI.plateCast(), 32),
+            new RecipesCommon.ComparableStack(GTDITems.wheel,3),
+            new RecipesCommon.ComparableStack(hbm:item.blade_titanium,8),
+            new RecipesCommon.ComparableStack(ModItems.motor, 8),
+            new RecipesCommon.OreDictStack(STEEL.lightBarrel(), 4),
+            new RecipesCommon.ComparableStack(ModItems.weapon_mod_special, 2,GunFactory.EnumModSpecial.ENGINE_DIESEL)
+        ));
     }
 }
